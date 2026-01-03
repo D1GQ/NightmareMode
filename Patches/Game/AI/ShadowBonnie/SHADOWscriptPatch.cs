b53b129c19caf770d0ffcde60fc0c5ac72cebd4d
D1GQ <yourboylax@gmail.com>
2026-01-02 19:16:37 -0600

    Remove shadow bonnie

---
+++ Patches/Game/AI/ShadowBonnie/SHADOWscriptPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace NightmareMode.Patches.Game.AI.ShadowBonnie;
+
+[HarmonyPatch(typeof(SHADOWscript))]
+internal class SHADOWscriptPatch
+{
+    // Remove Mini Shadow Bonnies in cameras entirely in Nightmare Mode
+    [HarmonyPatch(nameof(SHADOWscript.Start))]
+    [HarmonyPrefix]
+    private static bool Start_Prefix(SHADOWscript __instance)
+    {
+        if (NightmarePlugin.ModEnabled)
+        {
+            UnityEngine.Object.Destroy(__instance.gameObject);
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Patches/Game/AI/ShadowBonnie/ShadowBonnieMainScriptPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace NightmareMode.Patches.Game.AI.ShadowBonnie;
+
+[HarmonyPatch(typeof(ShadowBonnieMainScript))]
+internal class ShadowBonnieMainScriptPatch
+{
+    // Disable Shadow Bonnie in Nightmare Mode
+    [HarmonyPatch(nameof(ShadowBonnieMainScript.Update))]
+    [HarmonyPrefix]
+    private static bool Update_Prefix()
+    {
+        if (NightmarePlugin.ModEnabled)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}

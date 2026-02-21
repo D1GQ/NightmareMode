d6f8b0bb2f973ffb24474e2c108e6d60157e4a49
D1GQ <yourboylax@gmail.com>
2026-02-20 21:50:19 -0600

    bump ver

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -4,7 +4,7 @@ internal static class ModInfo
 {
     internal const string MOD_GUID = "com.d1gq.nightmaremode";
     internal const string MOD_NAME = "NightmareMode";
-    internal const string MOD_VERSION = "2.0.1";
+    internal const string MOD_VERSION = "2.1.0";
 #if DEBUG
     internal const bool DEBUG = true;
 #else
